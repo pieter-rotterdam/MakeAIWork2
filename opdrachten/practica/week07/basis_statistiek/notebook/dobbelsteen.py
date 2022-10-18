6d7d93a1a24125f8f57b1c8c3a84b2ea523b9a78
pieter-rotterdam <0757747@hr.nl>
2022-10-18 19:08:44 +0200

    dinsdagavond

--- opdrachten/practica/week07/basis_statistiek/notebook/dobbelsteen.py
+++ opdrachten/practica/week07/basis_statistiek/notebook/dobbelsteen.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import random
+import numpy as np
 
 class Dobbelsteen:
     
     def __init__(self):
         self.values = set(range(1, 7))  
         self.roll()
+        self.history()
+        self.getHistory()
         
         # https://realpython.com/python-dice-roll
         self.faces = {
@@ -80,10 +83,17 @@ class Dobbelsteen:
 
     def show(self):        
         return str(self.faces.get(self.number))
-
+    
+    def history(self):
+        self.history = "2"
+    
+    def getHistory(self):
+        return self.history
+        
 def main():
     d = Dobbelsteen()
     d.roll()
+    d.history()
     d.show()
 
 if __name__ == main():
