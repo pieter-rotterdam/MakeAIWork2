5e64dadd8ef75c236ab4a0c6a08104ffd5e8cec6
pieter-rotterdam <0757747@hr.nl>
2022-11-15 19:49:58 +0100

    had a chat with tim apple

--- projects/apple_disease_classification/8_2Chatbot.py
+++ projects/apple_disease_classification/8_2Chatbot.py
@@ -1,5 +1,6 @@
+# start this file from folder projects/apple_disease_classification for json file to open correctly
 import nltk
-nltk.download('punkt')
+# nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
@@ -7,58 +8,67 @@ import numpy
 import tflearn
 import tensorflow as tf
 import random
-
 import json
+import pickle
+
 with open('../../jsonfile/intents.json') as file:
     data = json.load(file)
 
-print (data["intents"])
+# print (data['intents'])
+#pickle and try checks if there's already a model and data only runs code if there isn't
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+# try:
+#     with open('data.pickle', 'rb') as pfile:
+#         words, labels, training, output = pickle.load(pfile)
+# except: 
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data['intents']:
-    for pattern in intent['patterns']:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-        
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])
+    for intent in data['intents']:
+        for pattern in intent['patterns']:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent['tag'])
+            
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"] #turn everything in lower case
-words = sorted(list(set(words))) #remove duplicates
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"] #turn everything in lower case
+    words = sorted(list(set(words))) #remove duplicates
 
-labels = sorted(labels)
+    labels = sorted(labels)
 
-training = []
-output = [] #this is the text in json file that also needs one hot encoding
+    training = []
+    output = [] #this is the text in json file that also needs one hot encoding
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-    wrds = [stemmer.stem(w.lower()) for w in doc]
+        wrds = [stemmer.stem(w.lower()) for w in doc]
 
-    for w in words: #if word exists add to bag, else 0
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        for w in words: #if word exists add to bag, else 0
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    output_row = out_empty[:] #copy of out empty
-    output_row[labels.index(docs_y[x])] = 1 #look through labels list, see where tag is and set value to 1 in output row
+        output_row = out_empty[:] #copy of out empty
+        output_row[labels.index(docs_y[x])] = 1 #look through labels list, see where tag is and set value to 1 in output row
 
-    training.append(bag)
-    output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
 
-training = numpy.array(training) #confersion to array for tflearn
-output = numpy.array(output)
+    training = numpy.array(training) #conversion to array for tflearn
+    output = numpy.array(output)
+
+    # with open('data.pickle', 'wb') as pfile:
+    #     pickle.dump((words, labels, training, output), pfile)
 
 # tf.reset_default_graph() #see if this is replaced by something else?
 
@@ -70,5 +80,41 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+def chat():
+    print ("Tim Apple helps you with questions on the Apple Quality Control (type quit to stop) ")
+    while True:
+        inp = input('You: ')
+        if inp.lower() == 'quit':
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data['intents']:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
+
+chat()
+
