aad30b54b9d85543b000d8c69c5a3b1bb26e6eb8
pieter-rotterdam <0757747@hr.nl>
2022-11-16 21:12:56 +0100

    chatbot trained with apple stats

--- projects/apple_disease_classification/8_2_copy Chatbot.py
+++ projects/apple_disease_classification/8_2_copy Chatbot.py
@@ -6,7 +6,7 @@ stemmer = LancasterStemmer()
 
 import numpy
 import tflearn
-import tensorflow as tf
+import tensorflow
 import random
 import json
 import pickle
@@ -14,35 +14,33 @@ import pickle
 with open('../../jsonfile/intents.json') as file:
     data = json.load(file)
 
-# print (data['intents'])
-#pickle and try checks if there's already a model and data only runs code if there isn't
-
 try:
-    with open('data.pickle', 'rb') as pfile:
-        words, labels, training, output = pickle.load(pfile)
-except: 
+    x
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
     words = []
     labels = []
     docs_x = []
     docs_y = []
 
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
             wrds = nltk.word_tokenize(pattern)
             words.extend(wrds)
             docs_x.append(wrds)
-            docs_y.append(intent['tag'])
-            
-        if intent['tag'] not in labels:
-            labels.append(intent['tag'])
+            docs_y.append(intent["tag"])
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"] #turn everything in lower case
-    words = sorted(list(set(words))) #remove duplicates
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
     labels = sorted(labels)
 
     training = []
-    output = [] #this is the text in json file that also needs one hot encoding
+    output = []
 
     out_empty = [0 for _ in range(len(labels))]
 
@@ -51,26 +49,26 @@ except:
 
         wrds = [stemmer.stem(w.lower()) for w in doc]
 
-        for w in words: #if word exists add to bag, else 0
+        for w in words:
             if w in wrds:
                 bag.append(1)
             else:
                 bag.append(0)
 
-        output_row = out_empty[:] #copy of out empty
-        output_row[labels.index(docs_y[x])] = 1 #look through labels list, see where tag is and set value to 1 in output row
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
         training.append(bag)
         output.append(output_row)
 
 
-    training = numpy.array(training) #conversion to array for tflearn
+    training = numpy.array(training)
     output = numpy.array(output)
 
-    # with open('data.pickle', 'wb') as pfile:
-    #     pickle.dump((words, labels, training, output), pfile)
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
-# tf.reset_default_graph() #see if this is replaced by something else?
+# tensorflow.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
@@ -83,6 +81,7 @@ model = tflearn.DNN(net)
 try:
     model.load("model.tflearn")
 except:
+
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
@@ -100,21 +99,19 @@ def bag_of_words(s, words):
     return numpy.array(bag)
 
 def chat():
-    print ("Tim Apple helps you with questions on the Apple Quality Control (type quit to stop) ")
+    print("Start talking with the bot (type quit to stop)!")
     while True:
-        inp = input('You: ')
-        if inp.lower() == 'quit':
+        inp = input("You: ")
+        if inp.lower() == "quit":
             break
 
         results = model.predict([bag_of_words(inp, words)])
         results_index = numpy.argmax(results)
         tag = labels[results_index]
 
-        for tg in data['intents']:
+        for tg in data["intents"]:
             if tg['tag'] == tag:
                 responses = tg['responses']
 
         print(random.choice(responses))
-
 chat()
-
