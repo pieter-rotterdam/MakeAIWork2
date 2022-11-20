08b8b66acab3b9ef2a086280ff90507b97b22592
pieter-rotterdam <0757747@hr.nl>
2022-11-20 22:40:22 +0100

    some tuning 91/120 correct so far

---
+++ projects/apple_disease_classification/test.py
@@ -0,0 +1,3 @@
+https://towardsdatascience.com/transfer-learning-with-fruit-classification-a7a38e3f93b4
+
+https://ashleyycz.medium.com/how-i-made-a-i-to-detect-rotten-produce-using-a-cnn-f2f16a316914
