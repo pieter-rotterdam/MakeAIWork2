e90e125fd9943348bd2036b49081828fc2f696bf
pieter-rotterdam <0757747@hr.nl>
2022-11-23 21:29:11 +0100

    test copy - tested random samples -minor bug

--- projects/apple_disease_classification/test.py
+++ projects/apple_disease_classification/test.py
@@ -14,7 +14,15 @@ model = tf.keras.models.load_model(
        ('./models/appleclassifier73transfer.h5'),
        custom_objects={'KerasLayer':hub.KerasLayer})
 
-yhat = model.predict(useBatches)
-pred = (np.argmax(yhat, axis=-1))
+batch1 = model.predict(useBatches)[:80]
+pred1 = (np.argmax(batch1, axis=-1))
 
-print (pred)
+batch2 = model.predict(useBatches)[80:160]
+pred2 = (np.argmax(batch2, axis=-1))
+
+batch3 = model.predict(useBatches)[160:240]
+pred3 = (np.argmax(batch2, axis=-1))
+
+
+
+print (pred1)
