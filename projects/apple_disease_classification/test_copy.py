b8cd7017daa8222627bc23c0fa9fa9b7a597e164
pieter-rotterdam <0757747@hr.nl>
2022-11-24 21:40:44 +0100

    updated chat colors and intents json

--- projects/apple_disease_classification/test_copy.py
+++ projects/apple_disease_classification/test_copy.py
@@ -1,6 +1,6 @@
 import colorama 
 colorama.init()
-from colorama import Fore, Style
+from colorama import Fore, Back, Style
 import json 
 import os
 import numpy as np
@@ -42,7 +42,8 @@ model = tf.keras.models.load_model(
        ('./models/appleclassifier73transfer.h5'),
        custom_objects={'KerasLayer':hub.KerasLayer})
 
-print ("Model Loaded, please wait for the predictions")
+print (Fore.WHITE + Back.GREEN + "Model Loaded, please wait for the predictions")
+
 
 #make predictions and stats
 predList = []
@@ -66,7 +67,7 @@ for x in list_samples:
     pred = (np.argmax(batch, axis=-1))  
     # print(f'Sample: {x} prediction: {pred} ')
     predList.append(pred)
-    print(f'predictions {x} of 10 done.')
+    print(Fore.WHITE + Back.BLUE + f'predictions {x} of 10 done.')
 
 for pred in predList:
     unique2, counts2 = np.unique(pred, return_counts=True)
@@ -85,15 +86,15 @@ for pred in predList:
     nbrRej = int(nbrBlotch)+int(nbrRot)+int(nbrScab)
     nbrRejList.append(nbrRej)
 
-    percBlotch = int(nbrBlotch)/int(nbrInBatch)*100
+    percBlotch = round(int(nbrBlotch)/int(nbrInBatch)*100,2)
     percBlotchList.append(percBlotch)
-    percNormal = int(nbrNormal)/int(nbrInBatch)*100
+    percNormal = round(int(nbrNormal)/int(nbrInBatch)*100,2)
     percNormalList.append(percNormal)
-    percRot = int(nbrRot)/int(nbrInBatch)*100
+    percRot = round(int(nbrRot)/int(nbrInBatch)*100,2)
     percRotList.append(percRot)
-    percScab = int(nbrScab)/int(nbrInBatch)*100
+    percScab = round(int(nbrScab)/int(nbrInBatch)*100,2)
     percScabList.append(percScab)
-    percRej = int(nbrRej)/int(nbrInBatch)*100
+    percRej = round(int(nbrRej)/int(nbrInBatch)*100,2)
     percRejList.append(percRej)
 
     if nbrInBatch != 80:
@@ -111,7 +112,10 @@ for pred in predList:
     batchStatusList.append(batchStatus)
 
 print (nbrNormalList)
+print (nbrNormalList[1])
 print (batchStatusList)
+
+
 
 def responseReplacer(response):
     response = response.replace("{nbrInBatch}", str(nbrInBatch))
@@ -131,7 +135,7 @@ def responseReplacer(response):
 def chat():
     # load trained model
     model = keras.models.load_model('sequentialChatModel')
-    print(Fore.YELLOW + "Start messaging with AQL assistant Tim Apple (type quit to stop)!" + Style.RESET_ALL)
+    print(Fore.WHITE + Back.RED + "Start messaging with AQL assistant Tim Apple (type quit to stop)!" + Style.RESET_ALL)
     # load tokenizer object
     with open('tokenizer.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
@@ -145,7 +149,7 @@ def chat():
     inp, stopCondition = "", ""
 
     while not stopCondition:
-        print(Fore.LIGHTMAGENTA_EX + "PinkLady User: " + Style.RESET_ALL, end="")
+        print(Fore.WHITE + Back.LIGHTMAGENTA_EX + "PinkLady User: " + Style.RESET_ALL, end="")
         inp = input()
         stopCondition = inp.lower() in ["quit", "exit", "stop"]
 
@@ -165,7 +169,7 @@ def chat():
         for tg in data['intents']:
             if tg['tag'] == tag:
                 responses = [responseReplacer(response) for response in tg['responses']]
-                print(Fore.GREEN + "AQL assistant Tim Apple:" + Style.RESET_ALL , np.random.choice(list(responses)))
+                print(Fore.WHITE+ Back.LIGHTBLUE_EX + "AQL assistant Tim Apple:" + Style.RESET_ALL , np.random.choice(list(responses)))
 
         # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
 chat()
